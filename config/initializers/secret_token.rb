5af75085a42bd404d219619abcb5462fdaed7484
Bulent Tastan <bulenttastan@gmail.com>
2012-05-03 17:40:57 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picwar::Application.config.secret_token = '4fbdb6564fd5876972751c4326b3b7d7fd705135e31f18fd6fc0b90fb63879d6b2797f1fdb4c727c6047d5791f50e7c1caabe0be5c3c6760a68b237685cb63f3'
